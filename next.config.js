0c1df41992c36dbf14dbe2c35652b684bbba8c98
bridgerz <bridgerzoske@gmail.com>
2023-10-04 10:47:16 -0700

    update vercel.json

--- next.config.js
+++ next.config.js
@@ -2,36 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  headers: [
-    {
-      source:
-        "https://thegraph.com/hosted-service/subgraph/dplace-org/dplace-testnet/*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "*",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value: "*",
-        },
-      ],
-    },
-  ],
-  headers: [
-    { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" },
-    {
-      key: "Access-Control-Allow-Methods",
-      value: "*",
-    },
-    {
-      key: "Access-Control-Allow-Headers",
-      value: "*",
-    },
-  ],
 }
 
 module.exports = nextConfig
