084ebeee8aae7f253435dd8bdde69a2e475444a3
bridgerz <bridgerzoske@gmail.com>
2023-10-04 17:17:38 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,23 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
+  async rewrites() {
     return [
       {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        source: "/subgraph/:path*",
+        destination: `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/:path*`,
       },
     ]
   },
